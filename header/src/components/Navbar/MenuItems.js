cc13e863b34ff92d5c6741b0cf0242fee9b19605
Alberto Fruchi <fralbex@gmail.com>
2022-01-08 16:28:37 +0100

    First commit

---
+++ header/src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Marketplace',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Brands',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Search',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Login',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign Up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+]
